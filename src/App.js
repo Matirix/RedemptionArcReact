b7b74ee89002d30ffaf8a440b9d69cd9e7ff5a25
Matthew <Matirix@live.com>
2023-02-17 16:49:02 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1 className="text-4xl font-bold">
+          Template
+        </h1>
+    </div>
+  );
+}
+
+export default App;
